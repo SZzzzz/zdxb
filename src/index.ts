064ef0c59e471ef8a86f7dcd91f97bd6bc7469e6
SZ <353678810@qq.com>
2018-05-02 15:20:01 +0800

    feat: 添加模板.

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ const tm = new TemplateMaster(path.join(__dirname, '../templates'));
 tm.register('component', 'src/components');
 tm.register('model', 'src/models');
 tm.register('store', 'src/stores');
+tm.register('api', 'src/apis');
 export default tm;
